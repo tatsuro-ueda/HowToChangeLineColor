942771c353601c29d25735e37aa8728066e5a24b
Tatsuro Ueda <tatsuro.ueda@feel-physics.jp>
2018-11-03 16:29:40 +0900

    Initial commit

---
+++ Assets/DrawDynamicLine.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class DrawDynamicLine : MonoBehaviour {
+
+    public GameObject fromObject = null;
+    public GameObject toObject = null;
+    public GameObject lineRendererContainer = null;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        LineRenderer lineOld = lineRendererContainer.GetComponent<LineRenderer>();
+        DestroyImmediate(lineOld);
+
+        LineRenderer lineNew = lineRendererContainer.AddComponent<LineRenderer>();
+
+        lineNew.startColor = Color.blue;
+        lineNew.endColor = Color.red;
+        //第一引数には始点、第二引数には終点の色を渡します。
+
+        lineNew.startWidth = 0.1f;
+        lineNew.endWidth = 0.1f;
+
+        lineNew.SetPosition(0, fromObject.transform.position + new Vector3(0, -0.1f, 0));
+        lineNew.SetPosition(1, toObject.transform.position);
+        //line.SetPosition関数の第一引数は配列の要素数(配列は0スタートです！,第二引数は座標です)
+
+    }
+}
---
+++ Assets/Rotate.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    private GameObject centerOfRotation;
+    private float rotationSpeed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+        centerOfRotation = new GameObject();
+        centerOfRotation.transform.position = transform.position;
+        rotationSpeed = Random.value;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(
+            rotationSpeed * Mathf.Sin(Time.time),
+            rotationSpeed * Mathf.Cos(Time.time), 0) +
+            centerOfRotation.transform.position;
+	}
+}
